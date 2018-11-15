97cd5e23a27c6d718556749a8de2dbeabd0db2f2
A.D <adrianlebaron@gmail.com>
2018-11-15 09:58:44 -0700

    added a note to moment with url loots of cool stuff

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,24 +1,24 @@
 import moment from 'moment';
 
  // Epoch timestamp - number of seconds that have elapsed since January 1, 1970
- moment.now();
+//  moment.now();
 
- // Creating a new date object for right now
- moment();
+//  // Creating a new date object for right now
+//  moment();
 
  // Creating date object with a specific date/format
- const birthday = moment('1983-07-31', 'YYYY-MM-DD');
+ const birthday = moment('1984-08-03', 'YYYY-MM-DD');
 
- // Querying a named day of the week for a date
- console.log(birthday.format('dddd'));
+//  // Querying a named day of the week for a date
+//  console.log(birthday.format('dddd'));
 
- // Date counter
- birthday.fromNow();
+//  // Date counter
+  // console.log(birthday.fromNow());
 
- // Custom foramtted date
- birthday.format("MMM Do YYYY");
+//  // Custom foramtted date
+  console.log(birthday.format("MMM Do YYYY"));
 
- // Date calculations
- const twoWeeksFromNow = moment().add(14, 'days');
- const anotherTwoWeeksFromNow = moment().add(2, 'weeks');
- const sixMonthsAgo = moment().subtract(6, 'm'); 
+//  // Date calculations
+//  const twoWeeksFromNow = moment().add(14, 'days');
+//  const anotherTwoWeeksFromNow = moment().add(2, 'weeks');
+//  const sixMonthsAgo = moment().subtract(6, 'm'); 
